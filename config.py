315d28d477bae9c1424b2e65d8b0fdde07952f76
KimBrian <komenbowen@kabarak.ac.ke>
2026-03-03 21:23:38 +0300

    Create config.py file with configuration settings

---
+++ config.py
@@ -0,0 +1,15 @@
+# Configuration settings for Loan Prediction Model
+
+# Data paths
+TRAIN_DATA_PATH = 'data/train.csv'
+TEST_DATA_PATH = 'data/test.csv'
+
+# Feature lists
+FEATURES = ['feature1', 'feature2', 'feature3', 'featureN']
+
+# Model parameters
+MODEL_PARAMS = {
+    'param1': value1,
+    'param2': value2,
+    'param3': value3
+}
